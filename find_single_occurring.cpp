b4a292f5e807f93a30013c3c20a98c0571741e4d
Aditi Kulkarni <aditikulkarni@MAK.local>
2018-02-26 19:44:07 -0700

    updated

--- find_single_occurring.cpp
+++ find_single_occurring.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 
-int find_single_occurrence(int arr, int n)
+int find_single_occurrence(int arr[], int n)
 {
-    int single = 0;
+    int twos = 0, ones = 0;
+    int common_bit_mask;
     for(int i = 0; i < n; i++)
     {
-    	single ^= arr[i];
+    	twos = twos | (ones & arr[i]);
+        ones = ones ^ arr[i];
+        common_bit_mask = ~(ones & twos);
+        ones &= common_bit_mask;
+        twos &= common_bit_mask;
     }
-    return single;
+    return ones;
 }
 
 int main(int argc, char *argv[])
 {
-	int arr[] = {1, 3, 3, 9, 23, 1, 1, 1, 23};
-    int arr[] = {3, 3, 2, 3};
+	int arr[] = {3, 3, 3, 9, 23, 1, 1, 1, 23, 23};
+    // int arr[] = {3, 3, 2, 3};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	int forcout = find_single_occurence(arr, n);
+	int forcout = find_single_occurrence(arr, n);
 	std::cout << forcout << std::endl;
 	return 0;
 }
